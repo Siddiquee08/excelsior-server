2a213f166f81d38cbaecc264f1295eb3c811f707
T.M Siddiquee <siddiquee09@gmail.com>
2022-12-18 01:23:47 +0600

    api created

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 const courseCategories = require("./data/corseCategory.json");
+const allCourses = require("./data/courseDetails.json");
 app.get("/", (req, res) => {
   res.send("Server Up and Running");
 });
 app.get("/courseCategories", (req, res) => {
   res.send(courseCategories);
+});
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = allCourses.find((course) => course._id === id);
+  res.send(selectedCourse);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = allCourses.filter((c) => c.category_id === id);
+  res.send(course);
 });
 app.listen(port, () => {
   console.log("The server is running on port:", port);
